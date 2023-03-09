6919eaae7566394ff8f0d6c9eae96a581dd9aeaf
Yuri Rebrov <yurirebrov01@gmail.com>
2023-03-09 23:59:19 +0300

    create chat header & footer

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import { Avatar } from '@chakra-ui/react'
+import { Input, InputGroup, Button } from '@chakra-ui/react'
 
 const Chat = () => {
   return (
     <div className='flex flex-col justify-between h-full'>
-      <div>
-        <Avatar name='' size='md'/>
+      <div className='py-[15px] bg-text text-center'>
+        <span>Username Surname</span>
       </div>
       <div className='h-full'>
         main
       </div>
-      <div>
-        footer
+      <div className='flex justify-center pb-[50px] pt-[20px]'>
+        <div className='w-[70%]'>
+          <InputGroup>
+            <Input placeholder='Enter your message...'/>
+            <Button width='100px' marginLeft='10px'>Send</Button>
+          </InputGroup>
+        </div>
       </div>
     </div>
   )
